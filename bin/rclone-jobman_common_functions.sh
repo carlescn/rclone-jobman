4454744a2f93e516c94d4b24e8851a9ac499d440
carlescn <100298997+carlescn@users.noreply.github.com>
2023-02-01 22:04:14 +0100

    Add TUI to common functions and submenus.

--- bin/rclone-jobman_common_functions.sh
+++ bin/rclone-jobman_common_functions.sh
@@ -14,7 +14,7 @@ function exit_bad_usage() {        # EXIT CODE 1
 
 function exit_if_file_missing() {  # EXIT CODE 2
     if [[ ! -f "$1" ]]; then
-        echo "ERROR: Could not find file $1." >&2
+        error_box "ERROR: Could not find file $1."
         exit 2
     fi
 }
@@ -28,27 +28,44 @@ function read_job_file_line() {    # EXIT CODE 3
     local key=$2
     local value; value=$(awk -F'[= ]' '/^'"$key"'/ {print $2; exit}' "$file" )
     if [[ -z "$value" ]]; then
-        echo "ERROR: Key $key is missing in your configuration file, or it is empty." >&2
+        error_box "ERROR: Key $key is missing in your configuration file, or it is empty."
         exit 3
     fi
     echo "$value"
 }
 
+function message_box() {
+    local IFS=; local message="$*"
+    local box_height=$(( 6 + $# ))
+    whiptail --backtitle "${script_name:?}" --msgbox "$message" "$box_height" "${box_width:?}"
+}
+
+function error_box() {
+    local IFS=; local message="$*"
+    local box_height=$(( 6 + $# ))
+    whiptail --backtitle "${script_name:?}" --title "ERROR" --msgbox "$message" "$box_height" "${box_width:?}"
+    echo "$message" >&2
+}
+
 function ask_for_confirmation() {
-# Asks user to type YES or returns 1
-    local user_input
-    read -r -p "Type YES to confirm: " user_input
-    if [[ $user_input != YES ]]; then
-        echo -e "Process interrupted by the user."
+# Asks user for confirmation. Returns 1 if response is NO.
+    local IFS=; local message="$*"
+    local box_height=$(( 6 + $# ))
+    local title="This operation is IRREVERSIBLE. Are you sure?"
+    if whiptail --backtitle "${script_name:?}" --title "$title" --yesno "$message" "$box_height" "${box_width:?}" --defaultno; then
+        return 0
+    else
+        message_box "Process interrupted by the user. Returning to the menu..."
         return 1
     fi
 }
 
-function press_any_key() {
-    local user_input
-    read -rsn 1 -p "Press C to cancel, or any key to continue." user_input && echo ""
-    case $user_input in
-        c|C)  return 1 ;;
-        *)    return 0 ;;
-    esac
+function yes_no_dialog() {
+    local IFS=; local message="$*"
+    local box_height=$(( 6 + $# ))
+    if whiptail --backtitle "${script_name:?}" --title "Please confirm" --yesno "$message" "$box_height" "${box_width:?}" --defaultno; then
+        return 0
+    else
+        return 1
+    fi
 }
